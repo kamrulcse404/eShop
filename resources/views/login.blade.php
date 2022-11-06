ba33c3650d23d924d8d9ab9a25d5f5c9e0e4a1a7
kamrulcse404 <kamrul.h01960@gmail.com>
2022-11-07 01:57:55 +0600

    font awesome v5 added bootstrap jquery added before

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,5 @@
 @extends('master')
 @section('content')
     <h3>Login Page</h3>
-    <button class="btn btn-outline-primary">Click on me</button>
+    <button class="btn btn-outline-primary"><i class="fas fa-edit"></i> Click on me</button>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    {{-- jQuery  --}}
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"
         integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-
+    {{-- bootstrap  --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"></script>
     <title>eShop</title>
 </head>
 
@@ -23,12 +26,12 @@
     </script>
 </body>
 
-    <script>
-        $(document).ready(function(){
-            $("button").click(function(){
-                alert("all set")
-            })
+<script>
+    $(document).ready(function() {
+        $("button").click(function() {
+            alert("all set")
         })
-    </script>
+    })
+</script>
 
 </html>
